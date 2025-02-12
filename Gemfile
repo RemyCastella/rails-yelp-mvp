f8b1b6501da708dbe70c0ba2e06ff48396b08df5
Remy Castella <remy.jpen@gmail.com>
2025-02-12 16:53:53 +0900

    finish mvp

--- Gemfile
+++ Gemfile
@@ -59,3 +59,8 @@ end
 
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+gem "ostruct"
+
+gem "simple_form"
+
+gem "faker"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "faker"
+
+puts "Clearing restaurants and reviews tables"
+
+Review.delete_all
+Restaurant.delete_all
+
+puts "Creating restaurants..."
+30.times do
+  new_restaurant = Restaurant.create!({
+    address: Faker::Address.street_address,
+    name: Faker::Restaurant.name,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: Restaurant::CATEGORIES.sample
+  })
+end
+puts "...Created #{Restaurant.count} restaurants"
+
+puts "Creating reviews..."
+200.times do
+  new_review = Review.create!({
+    rating: (1..5).to_a.sample,
+    content: Faker::Restaurant.review,
+    restaurant: Restaurant.all.sample
+  })
+end
+puts "...#{Review.count} reviews created"
